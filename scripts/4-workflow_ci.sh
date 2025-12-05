7a539ffb2781979f6a72eb665342f54463094bd1
Haytem Alsharif <haytem.alsharif@effectual.com>
2025-12-05 22:44:43 +0000

    wep

--- scripts/4-workflow_ci.sh
+++ scripts/4-workflow_ci.sh
@@ -74,11 +74,10 @@ jobs:
           exit-code: 1
           severity: CRITICAL,HIGH
 
-      - name: Plan
+      - name: Terraform Plan
         run: terraform plan -input=false -out=plan.tfplan
 
-      - name: Show Plan (for PR readability)
-        if: github.event_name == 'pull_request'
+      - name: Saving Plan as Artifact
         run: terraform show -no-color plan.tfplan > plan.txt
 
       - name: Upload Plan Artifact
@@ -86,62 +85,10 @@ jobs:
         with:
           name: plan
           path: plan.txt
+          
+      - name: TF Apply
+        run: terraform apply plan.tfplan -no-color 
+        if: github.ref == 'refs/heads/main'
 CI
 
-
-### Create a GitHub Actions workflow file for Terraform APPLY
-# echo "#####  - .github/workflows/terraform-apply.yml"
-# cat > .github/workflows/terraform-apply.yml <<'APPLY'
-# name: terraform-apply
-# on:
-#   push:
-#     branches: [main]
-
-# permissions:
-#   contents: read
-#   id-token: write
-
-# concurrency:
-#   group: ${{ github.workflow }}-main
-#   cancel-in-progress: false
-
-# env:
-#   TF_IN_AUTOMATION: "true"
-#   AWS_REGION: ${{ vars.AWS_REGION }}
-#   TF_BACKEND_BUCKET: ${{ vars.TF_BACKEND_BUCKET }}
-#   TF_BACKEND_KEY: ${{ vars.TF_BACKEND_KEY }}
-#   TF_BACKEND_DDB_TABLE: ${{ vars.TF_BACKEND_DDB_TABLE }}
-#   AWS_ROLE_ARN: ${{ secrets.AWS_ROLE_ARN }}
-
-# jobs:
-#   apply:
-#     runs-on: ubuntu-latest
-#     # environment: production  # optional: gate with environment approvals
-
-#     steps:
-#       - uses: actions/checkout@v4
-
-#       - name: Configure AWS credentials (OIDC)
-#         uses: aws-actions/configure-aws-credentials@v4
-#         with:
-#           role-to-assume: ${{ env.AWS_ROLE_ARN }}
-#           aws-region: ${{ env.AWS_REGION }}
-
-#       - name: Setup Terraform
-#         uses: hashicorp/setup-terraform@v3
-#         with:
-#           terraform_wrapper: false
-
-#       - name: Terraform Init (S3 backend)
-#         run: |
-#           terraform init \
-#             -backend-config="bucket=${TF_BACKEND_BUCKET}" \
-#             -backend-config="key=${TF_BACKEND_KEY}" \
-#             -backend-config="region=${AWS_REGION}" \
-#             -backend-config="dynamodb_table=${TF_BACKEND_DDB_TABLE}"\
-#             -backend-config="encrypt=true"
-
-#       - name: Terraform Apply
-#         run: terraform apply -input=false -auto-approve
-# APPLY
 cd ..
